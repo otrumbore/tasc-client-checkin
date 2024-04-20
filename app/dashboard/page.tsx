5c3d1ecaa9cad47aee24456d371e6287afb5715d
Odnel Trumbore <otrumbore@gmail.com>
2024-04-19 20:41:39 -0400

    dashboard design updates

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -121,7 +121,7 @@ const Dashboard = () => {
 		},
 	];
 	return (
-		<div className='min-h-screen max-h-screen min-w-screen bg-[#003873] p-12 flex flex-row gap-4'>
+		<div className='min-h-screen max-h-screen min-w-screen bg-[#003873] lg:p-12 flex flex-row gap-4'>
 			{/* Side bar
 			<div className='w-2/12 p-4 bg-[#F47940] border-r-2 border-gray-700 rounded-md shadow-md shadow-black'>
 				<ul>
@@ -130,7 +130,7 @@ const Dashboard = () => {
 					<li>Scheduled</li>
 				</ul>
 			</div> */}
-			<div className='bg-white w-full max-h-screen p-4 rounded-lg shadow-lg shadow-black overflow-auto'>
+			<div className='bg-white w-full max-h-screen p-2 lg:p-4 rounded-lg shadow-lg shadow-black'>
 				<div className='flex w-full justify-between items-center px-4 text-lg'>
 					<div className='w-1/3'>Hello, Admin</div>
 					<PageHeading />
@@ -139,7 +139,7 @@ const Dashboard = () => {
 
 				{/* Main */}
 				<div className='w-full p-4'>
-					<div className='flex flex-col gap-4'>
+					<div className='flex flex-col gap-4 h-full max-h-[75vh] overflow-auto'>
 						<div className='border-2 border-[#F47940] rounded-md w-full grid grid-cols-6 p-4 text-xl'>
 							<p className='font-bold'>Check In Time</p>
 							<p className='font-bold'>Queue #</p>
@@ -148,7 +148,7 @@ const Dashboard = () => {
 							<p className='font-bold'>Last 4 SSN</p>
 							<p className='font-bold'>Actions</p>
 						</div>
-						<div className='border-2 border-[#F47940]/90 rounded-md'>
+						<div className='border-2 border-[#F47940]/90 rounded-md h-full overflow-auto'>
 							{clients.map((client) => (
 								<div
 									className={`${
